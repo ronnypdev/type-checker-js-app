351e312a397bc98deb4dc74db1029de265f14020
Ronny Perez <ronnypdev@gmail.com>
2020-09-21 11:46:37 -0400

    Two way typing with Javascript

--- js/app.js
+++ js/app.js
@@ -1,9 +1,24 @@
 const sentenceTag = document.querySelector(`input[type='text']`);
 const outputTag = document.querySelector('.output');
+const originalText = outputTag.value;
 
 // when i type in my sentence tag, update the output tag accordingly
-function outPutValue(eve) {
-  outputTag.innerHTML = eve.target.value;
+// if there's no value, put in the original text
+function sentenceValue(eve) {
+  if (eve.target.value) {
+    outputTag.value = eve.target.value;
+  } else {
+    outputTag.value = originalText;
+  }
 }
 
-sentenceTag.addEventListener('input', outPutValue, false);
+// When i type in my output tag, update the sentence tag accordingly
+function outPutValue(e) {
+  sentenceTag.value = e.target.value;
+}
+
+// sentenceValue
+sentenceTag.addEventListener('keyup', sentenceValue, false);
+
+// outPutValue
+outputTag.addEventListener('keyup', outPutValue, false);
